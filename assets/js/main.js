fa24769d9d729a1cbc6a284141e6fa65c15bee1d
Srijana Limbu <91357218+srijanalimbu55@users.noreply.github.com>
2024-02-27 07:59:53 +0545

    home

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,6 +20,15 @@ navClose.addEventListener('click', () =>{
 }
 
   /*====================REMOVE MENU MOBILE===================*/
+  const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
 
 
   /*====================QUESTIONS ACCORFION===================*/
